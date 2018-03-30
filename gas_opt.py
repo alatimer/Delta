a6c207115e5ca89e2f38187f4462b96fb2afcbe3
Allegra Latimer <alatimer@sherlock-ln04.stanford.edu>
2018-03-30 09:05:32 -0700

    finalized gas submission script, no errors

--- gas_opt.py
+++ gas_opt.py
@@ -4,7 +4,7 @@
 #SBATCH --output=myjob.out
 #SBATCH --error=myjob.err
 #SBATCH --time=15:00:00
-#SBATCH --mail-type=FAIL                            #get emailed about job BEGIN, END, or FAIL
+#SBATCH --mail-type=FAIL,END                            #get emailed about job BEGIN, END, or FAIL
 #SBATCH  --mail-user=allegralatimer@gmail.com
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=4000
@@ -23,7 +23,7 @@ import os
 import pdos,bader
 
 if len(sys.argv) < 3:
-    print "USAGE: sub_opt.py traj_file xc pw psppath"
+    print "USAGE: gas_opt.py xc pw psppath"
     exit()
 
 if os.path.exists('qn.traj')==True  and os.stat("qn.traj").st_size > 0:
@@ -55,7 +55,7 @@ calc = espresso(pw=pw,	#plane-wave cutoff
                 dw=dw,		#density cutoff
                 xc=xc,		#exchange-correlation functional
                 kpts=(1,1,1), #k-point sampling
-                nbands=-30,	#10 extra bands besides the bands needed to hold
+                nbands=-10,	#10 extra bands besides the bands needed to hold
                 					#the valence electrons
                 sigma=0.1,
                 psppath = psppath,
@@ -79,6 +79,23 @@ dyn = QuasiNewton(atoms, logfile='qn.log', trajectory='qn.traj')
 dyn.run(fmax=0.05)
 calc.stop()
 
+if xc == 'BEEF':
+    ens = BEEF_Ensemble(calc)
+    ens_e = ens.get_ensemble_energies()
+    ens.write('ensemble.bee')
+    pickle.dump(ens_e,open('ensemble.pkl','w'))
+
+####################### PDOS ###################
+
+pdos_traj = Trajectory('pdos_qn.traj','w',atoms)
+pdos.pdos(atoms,outdir=outdir,save_pkl=save_pdos_pkl,spinpol=spinpol)
+pdos_traj.write() #final image will have charges and duplicate geometry
+
+bader_traj = Trajectory('bader_qn.traj','w',atoms)
+bader.bader(atoms,outdir=outdir,save_cube=save_cube,save_cd=save_cd,spinpol=spinpol)
+bader_traj.write()
+
+################## VIBRATIONS ######################
 vibrateatoms=[]
 for i in range(len(atoms)):
         vibrateatoms.append(i)
@@ -111,19 +128,3 @@ vib.summary(method='standard')
 # Make trajectory files to visualize the modes.
 for mode in range(len(vibrateatoms)*3):
     vib.write_mode(mode)
-
-if xc == 'BEEF':
-    ens = BEEF_Ensemble(calc)
-    ens_e = ens.get_ensemble_energies()
-    ens.write('ensemble.bee')
-    pickle.dump(ens_e,open('ensemble.pkl','w'))
-
-####################### PDOS ###################
-
-pdos_traj = Trajectory('pdos_qn.traj','w',atoms)
-pdos.pdos(atoms,outdir=outdir,save_pkl=save_pdos_pkl,spinpol=spinpol)
-pdos_traj.write() #final image will have charges and duplicate geometry
-
-bader_traj = Trajectory('bader_qn.traj','w',atoms)
-bader.bader(atoms,outdir=outdir,save_cube=save_cube,save_cd=save_cd,spinpol=spinpol)
-bader_traj.write()
